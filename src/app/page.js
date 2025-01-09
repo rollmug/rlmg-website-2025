4cb61c694baa2e46b6d43e1b67c6997e5f88807d
Dave Kobrenski <dave@bbmdesigns.com>
2025-01-09 13:35:05 -0500

    test fonts

--- src/app/page.js
+++ src/app/page.js
@@ -12,7 +12,7 @@ export default function Home() {
       </div>
       <section className="p-6 pt-2 lg:p-10 lg:pt-2 max-w-2xl mx-auto ">
         <h2>Who We Are</h2>
-        <h3>We Create Experiences. With Purpose.</h3>
+        <h3>We Create Text. At Different Sizes.</h3>
         <hr />
         <p>
           We bring to life innovative and imaginative experiences that stir emotions. Shift perceptions. Provoke joy, wonder, debate. We open doors and cast light on who we are as humans, where we’ve been, where we’re going. We bring people together and explore what connects us. We create meaningful moments, and we do it with a sense of purpose every step of the way.
