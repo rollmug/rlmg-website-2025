48e930587ef39a6273d04e50cfbf8292a22b051b
Dave Kobrenski <dave@bbmdesigns.com>
2025-01-09 11:07:20 -0500

    initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,14 +1,10 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const nunitoSans = Nunito_Sans({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-nunito'
 });
 
 export const metadata = {
@@ -19,9 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${nunitoSans.className} antialiased`}>
         {children}
       </body>
     </html>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,11 +7,36 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'nunito': ['var(--font-nunito)'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "#019FF9", // for buttons
+          "primary-content": "#ffffff", // button text color
+          secondary: "#019FF9", //blue - for headers
+          "base-100": "#ffffff", // main background
+          "base-200": "#F3F4F7", // slightly darker background on some content blocks
+          "base-content": "#2A303E", // main text color
+          neutral: "#2A303E", // for dark bg on quote blocks and other content blocks
+          "neutral-content": "#ffffff", // text on dark bg
+          accent: "#FFD95C", // for links on dark background, and for play btn color
+          warning: "#FFD95C",
+        }
+      }
+    ]
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('daisyui'),
+  ],
 };
