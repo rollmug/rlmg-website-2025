924a484770025423784c226d59e37e923fa50f57
Dave Kobrenski <dave@bbmdesigns.com>
2025-01-11 17:23:23 -0500

    storybook brandURL

--- .storybook/appTheme.js
+++ .storybook/appTheme.js
@@ -3,7 +3,7 @@ import { create } from '@storybook/theming';
 export default create({
     base: 'light',
     brandTitle: 'RLMG 2025 Rebrand',
-    brandUrl: 'https://rlmg.com',
+    brandUrl: 'https://rollmug.github.io/rlmg-website-2025',
     brandImage: 'https://manage.rlmg.com/assets/eeefa485-73a3-4ba9-88ae-924ac5e19ca4?width=100',
     brandTarget: '_self',
     fontBase: '"Nunito Sans", sans-serif',
