4c415426cf904eb920419ec5bc1db44148961492
Dave Kobrenski <dave@bbmdesigns.com>
2025-01-11 10:39:29 -0500

    storybook init

---
+++ .storybook/preview.js
@@ -0,0 +1,19 @@
+/** @type { import('@storybook/react').Preview } */
+
+import '../src/app/globals.css';
+
+const preview = {
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
