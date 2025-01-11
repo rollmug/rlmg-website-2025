46fd90d2b0229062a3f9ed7d0418c63aa2ca686a
Dave Kobrenski <dave@bbmdesigns.com>
2025-01-11 17:06:38 -0500

    storybook to pages action

--- .storybook/appTheme.js
+++ .storybook/appTheme.js
@@ -6,4 +6,5 @@ export default create({
     brandUrl: 'https://rlmg.com',
     brandImage: 'https://manage.rlmg.com/assets/eeefa485-73a3-4ba9-88ae-924ac5e19ca4?width=100',
     brandTarget: '_self',
+    fontBase: '"Nunito Sans", sans-serif',
 });
