7f0e9ae73a6def2eebb74027f6b1360fbebb4720
Dave Kobrenski <dave@bbmdesigns.com>
2025-01-13 11:22:44 -0500

    navigation story complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,40 +1,51 @@
+'use client';
+import { Navbar } from "@/components/ui/Navbar";
 import Image from "next/image";
+
+import RLMG from '../stories/assets/rlmg-logo.png';
 
 export default function Home() {
   return (
     <div className="">
-      <div className="relative pt-6 pb-24 lg:pb-48 bg-neutral slanted">
-        <div className="max-w-2xl mx-auto p-6 lg:p-10">
-          <h1 className="">
-            Typography is Everything
-          </h1>
+      <Navbar img={RLMG} orgName="RLMG" listItems={[
+        { text: "About Us", url: "/" },
+        { text: "Our Work", url: "/" },
+        { text: "Our Team", url: "/" },
+        { text: "Careers", url: "/" },
+        { text: "Contact", url: "/" },
+      ]} fixed />
+
+      <main className="py-14 lg:py-20">
+        <div className="relative pt-6 pb-24 lg:pb-48 bg-neutral slanted">
+          <div className="max-w-2xl mx-auto p-6 lg:p-10">
+            <h1 className="">
+              Typography is Everything
+            </h1>
+          </div>
         </div>
-      </div>
-      <section className="p-6 pt-2 lg:p-10 lg:pt-2 max-w-2xl mx-auto ">
-        <h2>Who We Are</h2>
-        <h3>We Create Text. At Different Sizes.</h3>
-        <hr />
-        <p>
-          We bring to life innovative and imaginative experiences that stir emotions. Shift perceptions. Provoke joy, wonder, debate. We open doors and cast light on who we are as humans, where we’ve been, where we’re going. We bring people together and explore what connects us. We create meaningful moments, and we do it with a sense of purpose every step of the way.
-        </p>
-
-        <p>
-          <button className="btn btn-primary my-2">Learn More</button>
-        </p>
-
-        <div className="my-8">
-          <h4>Our Capabilities</h4>
-
-          <ul>
-            <li>Strategy & Concept</li>
-            <li>Design Development & Prototyping</li>
-            <li>Interactive Media</li>
-            <li>Film & Linear Media</li>
-            <li>Immersive Environments</li>
-            <li>Project Management</li>
-          </ul>
-        </div>
-      </section>
+        <section className="p-6 pt-2 lg:p-10 lg:pt-2 max-w-2xl mx-auto ">
+          <h2>Who We Are</h2>
+          <h3>We Create Text. At Different Sizes.</h3>
+          <hr />
+          <p>
+            We bring to life innovative and imaginative experiences that stir emotions. Shift perceptions. Provoke joy, wonder, debate. We open doors and cast light on who we are as humans, where we’ve been, where we’re going. We bring people together and explore what connects us. We create meaningful moments, and we do it with a sense of purpose every step of the way.
+          </p>
+          <p>
+            <button className="btn btn-primary my-2">Learn More</button>
+          </p>
+          <div className="my-8">
+            <h4>Our Capabilities</h4>
+            <ul>
+              <li>Strategy & Concept</li>
+              <li>Design Development & Prototyping</li>
+              <li>Interactive Media</li>
+              <li>Film & Linear Media</li>
+              <li>Immersive Environments</li>
+              <li>Project Management</li>
+            </ul>
+          </div>
+        </section>
+      </main>
 
 
     </div>
--- src/components/ui/Navbar.jsx
+++ src/components/ui/Navbar.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useMemo, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 import PropTypes from 'prop-types';
 import Link from 'next/link';
@@ -10,55 +11,128 @@ export const Navbar = ({ img = null, orgName, listItems, fixed = false }) => {
      * fallback text (no logo) from short name in cms
      * links list with urls
      * { pathname: '/', query: { path: '/story/ui-elements-navbar--navigation' }
+     * 
+     * TO DO:
+     * 
+     * rethink how to toggle the dropdown menu with animation
+     * and style the links to the right perhaps?
      */
+
+    const [mobileNavOpen, setMobileNavOpen] = useState(false);
 
     return (
         <>
-            <header className={`${fixed ? 'fixed' : 'absolute'} top-0 left-0 bg-white shadow-md flex w-screen`}>
-                <nav className="navbar max-w-screen-lg w-full mx-auto justify-between min-h-12 lg:min-h-16 py-0 lg:py-3">
+            <header className={`${fixed ? 'fixed' : 'absolute'} top-0 left-0 bg-white shadow-md flex w-screen z-[1]`}>
+                <nav className="navbar max-w-screen-lg w-full mx-auto justify-between min-h-14 lg:min-h-16 py-0 lg:py-3">
                     <div className="flex-1">
-                        <Link href="/" className="btn btn-link text-xl flex items-center py-[6px] lg:py-0">
-                            {img ? (
-                                <Image src={img} alt="RLMG Logo" width={40} height={40} className="h-full w-auto" />
-                            ) : <span className="text-lg">{orgName}</span>
-                            }
+                        <Link href="/" className="btn btn-link text-xl py-[6px] lg:py-0">
+                            <Logo img={img} orgName={orgName} />
                         </Link>
                     </div>
                     <div className="navbar-center hidden">
                         {/* optional centered content */}
                     </div>
                     <div className="flex-none">
-                        <details className="dropdown static lg:hidden">
-                            <summary tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                                <svg className="h-6 w-7 text-primary" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
-                                    <rect className="h-8 w-full" x="80" y="96" fill="currentColor" />
-                                    <rect className="h-8 w-full" x="80" y="240" fill="currentColor" />
-                                    <rect className="h-8 w-full" x="80" y="384" fill="currentColor" />
-                                </svg>
-                            </summary>
-                            <ul tabIndex={0} className="text-sm left-0 mt-0 w-full dropdown-content menu menu-sm bg-base-100 z-[1 p-0 shadow-md [&_li>*]:rounded-none">
-                                {listItems.map((item, index) => (
-                                    <li key={index} className="text-sm">
-                                        <Link href={item.url}>{item.text}</Link>
-                                    </li>
-                                ))}
-                            </ul>
-                        </details>
+                        <div className="dropdown static lg:hidden">
+                            <div tabIndex={0} role="button" className="btn btn-ghost text-center lg:hidden" onClick={() => setMobileNavOpen((pv) => !pv)}>
+                                <MenuIcon open={mobileNavOpen} />
+                            </div>
+                            <MobileLinks listItems={listItems} open={mobileNavOpen} />
+                        </div>
 
                         <ul className="menu menu-horizontal hidden lg:flex gap-12">
-                            {listItems.map((item, index) => (
-                                <li key={index} className="text-sm">
-                                    <Link href={item.url}>{item.text}</Link>
-                                </li>
-                            ))}
+                            <Links listItems={listItems} className="text-sm" />
                         </ul>
-
                     </div>
                 </nav>
             </header>
         </>
     );
 };
+
+const Logo = ({ img, orgName }) => {
+    return (
+        <AnimatePresence>
+            {img ? (
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    exit={{ opacity: 0 }}
+                    transition={{ ease: "easeIn", duration: 1 }}
+                    className="inline-flex max-h-full w-20"
+                >
+                    <Image src={img} alt="RLMG Logo" width={0} height={0} className="_max-h-full object-contain _w-auto" />
+                </motion.div>
+            ) : <span className="text-lg">{orgName}</span>
+            }
+        </AnimatePresence>
+    )
+}
+
+const MenuIcon = ({ open }) => {
+
+    if (open) {
+        return (
+            <svg className="h-7 w-7 text-primary" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g strokeWidth="0"></g>
+                <g strokeLinecap="round" strokeLinejoin="round"></g>
+                <g>
+                    <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M5.29289 5.29289C5.68342 4.90237 6.31658 4.90237 6.70711 5.29289L12 10.5858L17.2929 5.29289C17.6834 4.90237 18.3166 4.90237 18.7071 5.29289C19.0976 5.68342 19.0976 6.31658 18.7071 6.70711L13.4142 12L18.7071 17.2929C19.0976 17.6834 19.0976 18.3166 18.7071 18.7071C18.3166 19.0976 17.6834 19.0976 17.2929 18.7071L12 13.4142L6.70711 18.7071C6.31658 19.0976 5.68342 19.0976 5.29289 18.7071C4.90237 18.3166 4.90237 17.6834 5.29289 17.2929L10.5858 12L5.29289 6.70711C4.90237 6.31658 4.90237 5.68342 5.29289 5.29289Z"
+                        fill="currentColor"
+                    ></path>
+                </g>
+            </svg>
+        )
+    } else {
+        return (
+            <svg className="h-6 w-7 _mr-[3px] -translate-x-[3px] text-primary" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg">
+                <rect className="h-8 w-full" x="80" y="96" fill="currentColor" />
+                <rect className="h-8 w-full" x="80" y="240" fill="currentColor" />
+                <rect className="h-8 w-full" x="80" y="384" fill="currentColor" />
+            </svg>
+        )
+    }
+}
+
+const MobileLinks = ({ listItems, open }) => {
+    return (
+        <AnimatePresence mode="popLayout">
+            {open && (
+                <motion.div
+                    initial={{
+                        opacity: 0,
+                    }}
+                    animate={{
+                        opacity: 1,
+                    }}
+                    exit={{
+                        opacity: 0,
+                    }}
+                    className=""
+                >
+                    <ul tabIndex={0} className="left-0 mt-0 w-full absolute menu menu-mobile menu-lg bg-base-100 z-[1 p-0 shadow-md [&_li>*]:rounded-none items-stretch text-center">
+                        <Links listItems={listItems} />
+                    </ul>
+                </motion.div>
+            )}
+        </AnimatePresence>
+    );
+}
+
+const Links = ({ listItems, className }) => {
+    return (
+        <>
+            {listItems.map((item, index) => (
+                <li key={index} className={className}>
+                    <Link className="inline-block" href={item.url}>{item.text}</Link>
+                </li>
+            ))}
+        </>
+    );
+}
 
 Navbar.propTypes = {
     /** Image source for the logo */
