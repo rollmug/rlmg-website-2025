d39e9440ed7bf70d82e1e39dfbbb9d0f89a8402c
Dave Kobrenski <dave@bbmdesigns.com>
2025-03-13 10:31:01 -0400

    quote block padding fix

--- src/components/content-blocks/Quote.jsx
+++ src/components/content-blocks/Quote.jsx
@@ -4,21 +4,21 @@ import { ContentSection } from "../layout/ContentSection";
 import { MdFormatQuote } from "react-icons/md";
 
 export const Quote = ({ quoteText, sourceAuthor, sourceOrg, template = 'dark', className }) => {
-
+    // px-[10%] lg:px-[15%]
     return (
         <ContentSection className={`quote-section`}>
-            <div className={`quote-container py-10 lg:py-16 px-[10%] lg:px-[15%] ${template} ${className}`}>
+            <div className={`quote-container section-padded py-10 lg:py-16  ${template} ${className}`}>
 
                 <blockquote className={`quote pb-3`}>
                     <div className="flex flex-col md:flex-row md:flex-wrap justify-center">
                         <div className="w-14 h-14">
-                            <MdFormatQuote className="text-primary w-14 h-14 -scale-[1]" />
+                            <MdFormatQuote className="text-primary w-14 h-14 -scale-[1] -translate-x-2" />
                         </div>
 
-                        <p className={`quote-text ${template === 'dark' ? 'font-bold' : 'font-semibold'}  grow md:text-lg md:w-min md:max-w-prose px-3 mt-0 md:mt-4`}>{quoteText}</p>
+                        <p className={`quote-text ${template === 'dark' ? 'font-bold' : 'font-semibold'}  grow md:text-lg md:w-min md:max-w-prose _px-3 mt-0 md:mt-4`}>{quoteText}</p>
                         
                         <div className="self-end w-14 h-14 order-4 md:order-3">
-                            <MdFormatQuote className="text-primary w-14 h-14" />
+                            <MdFormatQuote className="text-primary w-14 h-14 translate-x-2" />
                         </div>
 
                         {/* md:pl-14 ml-3 */}
@@ -26,7 +26,7 @@ export const Quote = ({ quoteText, sourceAuthor, sourceOrg, template = 'dark', c
 
                         <div className="basis-full  order-3 md:order-4 flex flex-row justify-center">
                             <div className="w-14 hidden md:block"></div>
-                            <p className="quote-citation my-0 grow md:w-min md:max-w-prose md:text-lg !leading-tight px-3">
+                            <p className="quote-citation my-0 grow md:w-min md:max-w-prose md:text-lg !leading-tight _px-3">
                                 <span className="text-sm md:text-sm2">
                                     {sourceAuthor && <cite className={`quote-author ${template === 'dark' ? 'font-bold' : 'font-semibold'} not-italic`}>{sourceAuthor}</cite>}
                                     {sourceAuthor && sourceOrg && <span className="quote-source-divider"> | </span>}
