e9ab85d622327b42a8c7784b8a65d3ac5533b58b
Dave Kobrenski <dave@bbmdesigns.com>
2025-03-28 12:05:03 -0400

    type-o in storybook fix

--- src/components/ui/SocialMediaIcons.stories.js
+++ src/components/ui/SocialMediaIcons.stories.js
@@ -34,7 +34,6 @@ export const IconsGroup = {
                 platform: 'youtube',
                 displayText: 'YouTube',
                 url: 'https://www.youtube.com/@rlmgmedia',
-',
             },
         ]
     }
