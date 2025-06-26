015e7c792aacf28d35cf48775caf7eccc87985bc
Dave Kobrenski <dave@bbmdesigns.com>
2025-06-26 11:10:13 -0400

    fetchOptions: { cache: "force-cache" }

--- src/lib/client.js
+++ src/lib/client.js
@@ -13,7 +13,7 @@ export const { getClient, query, PreloadQuery } = registerApolloClient(() => {
       uri: process.env.GRAPHQL_URL,
       // you can disable result caching here if you want to
       // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
-      // fetchOptions: { cache: "no-store" },
+      fetchOptions: { cache: "force-cache" }, // default is "auto no cache" no-store
     }),
   });
 });
